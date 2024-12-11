e1f47fecfcbfd89da267e133e5ee52aaf60dd106
mengzhe <mengzhe@asia.edu.tw>
2024-12-11 14:07:32 +0800

    CH05

---
+++ CH05/ExtensionHelper/ExtensionHelper/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExtensionHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Welcome to Dotnet Tutorials";
+
+            int wordCount = sentence.GetWordCount();
+
+            Console.WriteLine($"Count ： {wordCount}");
+            Console.ReadKey();
+        }
+    }
+
+    public static class ExtensionHelper
+    {
+        public static int GetWordCount(this string str)
+        {
+            if (!String.IsNullOrEmpty(str))
+            {
+                return str.Split(' ').Length;
+            }
+            return 0;
+        }
+    }
+}
